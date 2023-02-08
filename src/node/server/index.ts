b3f122abbcf98f1bf34672aaa02cd903de33e95e
Hika1149 <hikari94854@gmail.com>
2023-02-08 23:41:49 +0800

    pluginContainer: resolveId,load and transform (call with pluginContext)

--- src/node/server/index.ts
+++ src/node/server/index.ts
@@ -1,12 +1,31 @@
 import connect from "connect";
-
 import { blue, green } from "picocolors";
 import { optimize } from "../optimizer";
+import { resolvePlugins } from "../plugins";
+import { createPluginContainer } from "../pluginContainer";
+
+export interface ServerContext {}
 
 export async function startDevServer() {
   const app = connect();
   const root = process.cwd();
   const startTime = Date.now();
+
+  /** resolve plugins & create pluginContainer */
+  const plugins = resolvePlugins();
+  const pluginContainer = createPluginContainer(plugins);
+  const serverContext = {
+    root,
+    app,
+    plugins,
+    pluginContainer,
+  };
+
+  for (const plugin of plugins) {
+    if (plugin.configServer) {
+      await plugin.configServer(serverContext);
+    }
+  }
 
   app.listen(3000, async () => {
     await optimize(root);

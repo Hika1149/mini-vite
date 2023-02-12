8060b5a0bd6b228bb805510dd5ec28b4ed88e32b
Hika1149 <hikari94854@gmail.com>
2023-02-13 00:47:58 +0800

    plugin: importAnalysis - overwrite importPath for deps and rel/abs path

---
+++ src/node/plugins/importAnalysis.ts
@@ -0,0 +1,48 @@
+import path from "path";
+import { init, parse } from "es-module-lexer";
+import { Plugin } from "../plugin";
+import { isJSRequest } from "../utils";
+import { BARE_IMPORT_RE, PRE_BUNDLE_DIR } from "../constants";
+import MagicString from "magic-string";
+
+export const importAnalysisPlugin = (): Plugin => {
+  return {
+    name: "m-vite:importAnalysis",
+
+    async transform(code, id) {
+      if (!isJSRequest(id)) {
+        return null;
+      }
+
+      await init;
+
+      const [imports] = parse(code);
+      const ms = new MagicString(code);
+      for (const importInfo of imports) {
+        const { s: modStart, e: modEnd, n: modSource } = importInfo;
+
+        if (!modSource) {
+          return null;
+        }
+        /** overwrite importPath of deps */
+        if (BARE_IMPORT_RE.test(modSource)) {
+          const bundlePath = path.join("/", PRE_BUNDLE_DIR, `${modSource}.js`);
+          ms.overwrite(modStart, modEnd, bundlePath);
+        } else if (modSource.startsWith(".") || modSource.startsWith("/")) {
+          /** should overwrite relative/absolute import path?
+           *  yes
+           *  - default extension
+           *  - alias plugin...
+           * */
+          //@ts-ignore
+          const resolvedId = await this.resolve(modSource, id);
+          if (resolvedId) {
+            ms.overwrite(modStart, modEnd, resolvedId.id);
+          }
+        }
+      }
+
+      return { code: ms.toString(), map: ms.generateMap() };
+    },
+  };
+};
--- src/node/plugins/index.ts
+++ src/node/plugins/index.ts
@@ -1,7 +1,8 @@
 import { Plugin } from "../plugin";
 import { resolvePlugin } from "./resolve";
 import { esbuildTransformPlugin } from "./esbuild";
+import { importAnalysisPlugin } from "./importAnalysis";
 
 export function resolvePlugins(): Plugin[] {
-  return [resolvePlugin(), esbuildTransformPlugin()];
+  return [resolvePlugin(), esbuildTransformPlugin(), importAnalysisPlugin()];
 }
